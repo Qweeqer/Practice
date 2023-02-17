66ae4f6b8faa6ea8229cf10bf5ccbc3a5122c025
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-02-17 21:02:17 +0200

    Summary

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@
 // let arr4 = arr2.concat(arr);
 // console.log(arr3, arr4);
 
-const numbers = [5, 10, 15, 20, 25];
+// //forEach
+// const numbers = [5, 10, 15, 20, 25];
 
-// Класичний for
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(`Індекс ${i}, значення ${numbers[i]}`);
-}
-// Метод перебирання forEach
-numbers.forEach(function (number, index) {
-  console.log(`Індекс ${index}, значення ${number}`);
-});
+// // Класичний for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Індекс ${i}, значення ${numbers[i]}`);
+// }
+// // Метод перебирання forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
+let arr = [1, 3, 5, "a", "b", 7, 9, "n", "Ac", "bC", 12, 111, 888, 66, 77, 17]
+const sortArr = [...arr].filter((a, b) => a - b);
+const sortArr2 = [...arr].sort((a, b) => a - b);
+
+console.log('sortArr', sortArr);
+console.log('sortArr2', sortArr2);
+// const students = ["Віка", 1, "Андрій", "Олег", "Юля", "Борис", "Катя"];
+
+// const inAlphabetOrder = [...students].sort((a, b) => a.localeCompare(b));
+// console.log(inAlphabetOrder); // ['Андрій', 'Борис', 'Віка', 'Катя', 'Олег', 'Юля']
+
+// const inReversedOrder = [...students].sort((a, b) => b.localeCompare(a));
+// console.log(inReversedOrder); // ['Юля', 'Олег', 'Катя', 'Віка', 'Борис', 'Андрій']
