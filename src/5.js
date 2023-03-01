65bdd71c0a976dc0924755bdf94bfb668443b48e
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-01 10:39:35 +0200

    Update 5.js

--- src/5.js
+++ src/5.js
@@ -9,13 +9,15 @@ const circles = document.querySelectorAll('.cirkle');
 
 circles.forEach(circle => {
   //2 - перебрати кожен елемент, і вибрати його data-anim значення
-  const animationClass = circle.getAttribute('data-anim');
+    // const animationClass = circle.getAttribute('data-anim');
+    // або
+    const animationClass = circle.dataset.anim;
   //3 - вибране значення додати як клас за допомогою classList до цього елемента.
   circle.classList.add(animationClass);
   //4 - Перевірка, чи застосувались анімації
-  console.log(circle.classList.contains(circle.getAttribute('data-anim')));
+  console.log(circle.classList.contains(animationClass));
 });
-
+// або
 //4 - Перевірка, чи застосувались анімації
 // circles.forEach(circle => {
 //   console.log(circle.classList.contains(circle.getAttribute('data-anim')));
