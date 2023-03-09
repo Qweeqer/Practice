f422359cddf05e91c09f040eda058763f378e2ef
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-09 22:17:02 +0200

    Summary

--- src/5.js
+++ src/5.js
@@ -5,18 +5,18 @@
 // - вибрати всі теги із класом cirkle
 // -------------------------------------------------------------------------------------
 //1 - вибрати всі теги із класом cirkle
-const circles = document.querySelectorAll('.cirkle');
+// const circles = document.querySelectorAll('.cirkle');
 
-circles.forEach(circle => {
+// circles.forEach(circle => {
   //2 - перебрати кожен елемент, і вибрати його data-anim значення
   // const animationClass = circle.getAttribute('data-anim');
   // або
-  const animationClass = circle.dataset.anim;
+  // const animationClass = circle.dataset.anim;
   //3 - вибране значення додати як клас за допомогою classList до цього елемента.
-  circle.classList.add(animationClass);
+  // circle.classList.add(animationClass);
   //4 - Перевірка, чи застосувались анімації
-  console.log(circle.classList.contains(animationClass));
-});
+  // console.log(circle.classList.contains(animationClass));
+// });
 // або
 //4 - Перевірка, чи застосувались анімації
 // circles.forEach(circle => {
