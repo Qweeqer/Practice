5b56e966f69f9ea616d82fe2bbd8e1f95fe7fd91
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-24 00:52:24 +0200

    Update weather.js

--- src/Weather/weather.js
+++ src/Weather/weather.js
@@ -30,10 +30,29 @@ const todayButton = document.querySelector('#today-button');
 const threeDaysButton = document.querySelector('#three-day-button');
 const sevenDaysButton = document.querySelector('#seven-day-button');
 
+// ********Запит для обраного**************************
+const getWeatherData = async city => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch weather data');
+  }
+  const data = await response.json();
+  return data;
+};
+// *******************END*********************************
 sevenDaysButton.addEventListener('click', () => {
   fetchWeatherData(7);
 });
-
+// *********************clearWeather*****************************************
+const clearWeather = () => {
+  const weatherContainer = document.querySelector('.weather-container');
+  if (weatherContainer) {
+    weatherContainer.innerHTML = '';
+  }
+};
+  // **********************************************************
 const fetchWeatherData = async (numDays, city = cityInput.value) => {
   const apiUrl = numDays === 1 ? apiUrlWeather : apiUrlForecast;
   if (!city) {
@@ -163,8 +182,10 @@ cityInput.addEventListener('change', async () => {
     return;
   }
 // Enter 
-cityInput.addEventListener('keyup', event => {
+cityInput.addEventListener('keypress', event => {
   if (event.key === 'Enter') {
+    event.preventDefault();
+    clearWeather(); // Видалення попередньої погоди
     fetchWeatherData(1);
   }
 });
@@ -175,6 +196,7 @@ cityInput.addEventListener('keyup', event => {
 
   if (!response.ok) {
     console.error('Не вдалося отримати дані погоди');
+    alert('Не вдалося отримати дані погоди');
     return;
   }
 
@@ -290,142 +312,7 @@ const cityNameElement = document.querySelector('.city-name');
 const updateCityName = newCityName => {
   cityNameElement.textContent = newCityName;
 };
+
+export { getWeatherData };
 // **************************************************************************************************
 
-// const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
-// const apiUrlWeather = 'https://api.openweathermap.org/data/2.5/weather';
-// const apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast';
-// const cityInput = document.querySelector('#city-input');
-// const weatherList = document.querySelector('#weather-list');
-// const todayButton = document.querySelector('#today-button');
-// const threeDaysButton = document.querySelector('#three-day-button');
-// const sevenDaysButton = document.querySelector('#seven-day-button');
-
-// sevenDaysButton.addEventListener('click', () => {
-//   fetchWeatherData(7);
-// });
-
-// const fetchWeatherData = async (numDays, city = cityInput.value) => {
-//   const apiUrl = numDays === 1 ? apiUrlWeather : apiUrlForecast;
-//   if (!city) {
-//     return;
-//   }
-
-//   const response = await fetch(
-//     `${apiUrl}?q=${city}&units=metric&appid=${apiKey}`
-//   );
-
-//   if (!response.ok) {
-//     console.error('Не вдалося отримати дані погоди');
-//     return;
-//   }
-
-//   const data = await response.json();
-//   weatherList.innerHTML = '';
-
-//   let daysToShow = 1;
-//   if (numDays === 3) daysToShow = 3;
-//   else if (numDays === 7) daysToShow = 7;
-
-//   let weatherData;
-//   if (numDays === 1) {
-//     weatherData = [data];
-//   } else {
-//     weatherData = data.list
-//       .filter((_, index) => index % 8 === 0)
-//       .slice(0, daysToShow);
-//   }
-
-//   weatherData.forEach(weather => {
-//     const date = new Date(weather.dt * 1000);
-//     const dateStr = date.toLocaleDateString('uk-UA', {
-//       weekday: 'short',
-//       month: 'short',
-//       day: 'numeric',
-//     });
-//     const timeStr = date.toLocaleTimeString('uk-UA', {
-//       hour: 'numeric',
-//       minute: 'numeric',
-//     });
-//     const tempStr = Math.round(weather.main.temp) + '°C';
-//     const humidityStr = weather.main.humidity + '%';
-//     const windStr = weather.wind.speed + 'м/с';
-//     const iconUrl = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`;
-
-//     const weatherItem = document.createElement('li');
-//     weatherItem.classList.add('WeatherWrapper'); // Додано клас
-//     weatherItem.innerHTML = `
-//   <h3 class="DateWrapper">${dateStr} ${timeStr}</h3>
-//   <img class="IconStyled" src="${iconUrl}" alt="значок погоди">
-//   <p class="DegStyled">Температура: ${tempStr}</p>
-//   <p>Вологість: ${humidityStr}</p>
-//   <p class="WindStyled">Швидкість вітру: ${windStr}</p>
-// `;
-
-//     weatherList.appendChild(weatherItem);
-//   });
-// };
-// const fetchCityByGeolocation = async (latitude, longitude) => {
-//   const response = await fetch(
-//     `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`
-//   );
-//   const data = await response.json();
-//   if (data.length > 0) {
-//     cityInput.value = data[0].name;
-//     fetchWeatherData(1);
-//   }
-// };
-
-// const initApp = async () => {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(
-//       position => {
-//         fetchCityByGeolocation(
-//           position.coords.latitude,
-//           position.coords.longitude
-//         );
-//       },
-//       error => {
-//         console.error(error);
-//         fetchWeatherData(1);
-//       }
-//     );
-//   } else {
-//     fetchWeatherData(1);
-//   }
-// };
-
-// todayButton.addEventListener('click', () => {
-//   fetchWeatherData(1);
-// });
-
-// threeDaysButton.addEventListener('click', () => {
-//   fetchWeatherData(3);
-// });
-
-// initApp();
-
-
-
-    //     weatherItem.innerHTML = `
-    //   <h3 class="DateWrapper">${dateStr} ${timeStr}</h3>
-    //   <div class="city-marker-container">
-    //     <svg class="city-marker" width="14" height="24" viewBox="0 0 14 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    //       <path d="M7 0C3.14038 0 0 3.14038 0 7C0 11.7 7 24 7 24C7 24 14 11.7 14 7C14 3.14038 10.8596 0 7 0ZM7 10.5C5.06863 10.5 3.5 8.93137 3.5 7C3.5 5.06863 5.06863 3.5 7 3.5C8.93137 3.5 10.5 5.06863 10.5 7C10.5 8.93137 8.93137 10.5 7 10.5Z" />
-    //     </svg>
-    //     <h4 class="city-name">${cityName}</h4>
-    //   </div>
-    //   <img class="IconStyled" src="${iconUrl}" alt="значок погоди">
-    //   <p class="DegStyled">Температура: ${tempStr}</p>
-    //   <p>Вологість: ${humidityStr}</p>
-    //   <p class="WindStyled">Швидкість вітру: ${windStr}</p>
-    //   ${numDays === 1 ? `<p>Тиск: ${weather.main.pressure}</p>` : ''}
-    //   ${numDays === 1 ? `<p>Видимість: ${weather.main.visibility}</p>` : ''}
-    //   ${numDays === 1 ? `<p>Стан неба: ${weather.weather[0].skyStatus}</p>` : ''}
-    //   ${numDays === 1 ? `<p>Індекс УФ-випромінювання: ${weather.uvi}</p>` : ''}
-    //   ${
-    //     numDays === 1
-    //       ? `<p>Ймовірність опадів: ${weather.weather[0].precipitation}</p>`
-    //       : ''
-    //   }
-    // `;
